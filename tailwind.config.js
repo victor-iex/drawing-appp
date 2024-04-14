316a896aa00fc6ece425a2273f9845fd2f60c837
victor-iex <johnvictor.k.j@gmail.com>
2024-04-15 01:59:59 +0530

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+module.exports = {
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'teal': '#134e4a',
+      'navy': '#102C57',
+      'beige': '#DAC0A3',
+      'beige2': '#DAC0A3',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
+  },
+}
